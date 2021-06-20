805e8c90f84a2e710abb6915dd24948776f5e8cc
root <root@MiLaptop.localdomain>
2021-06-20 10:09:26 -0500

    Agregado nodemon

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const colors = require("colors");
 const port = 7878;
 const host ="192.168.1.79";
-/*
-const http = require("http");
-const server=http.createServer((req, res)=>{
-    res.status =200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end("Hello WORD");
-});
-server.listen(port, host, ()=>{
-    console.log(`Servidor en`, `${host}:${port}`.green);
-});
-*/
 
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
     res.send("Hola Mundo");
+});
+app.get("/about", (req, res)=>{
+    res.send("About me");
 });
 
 app.listen(port, host, ()=>{
